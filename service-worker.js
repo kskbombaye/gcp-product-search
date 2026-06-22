330b1d58e05ad8c960fcd91f1ac89f61798b2f0c
kskbombaye <167258312+kskbombaye@users.noreply.github.com>
2026-06-22 22:23:54 +0900

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "product-app-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./script.js",
+  "./master.csv",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
